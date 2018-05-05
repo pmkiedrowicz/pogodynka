9651c7540e9afbddb9977177b7dc15fca31b0499
Laures <laures666@wp.pl>
2018-05-05 14:20:37 +0200

    Adding mysql, lombok dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
-import static  spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
 
 public class Main {
     public static void main(String[] args) {
+        port(8080);
         get("/hello", (req, res) -> "Hello World");
-
-
-
-
-
-
-
-
-        stop();
+        // http://localhost:8080/sensor?temperature=22.4&humidity=33
+        post("/sensor", (request, response) -> {
+            String temperature = request.queryParams("temperature");
+            String humidity = request.queryParams("humidity");
+            return "Podana temperatura=" + temperature + ", wilgotność=" + humidity;
+        });
     }
 }
