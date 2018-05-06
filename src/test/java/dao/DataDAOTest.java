fe4b3d70c73cf5e949fd7380308d9f273e0f73d4
hakier <pmkiedrowicz@gmail.com>
2018-05-06 13:33:50 +0200

    up tests

--- src/test/java/dao/DataDAOTest.java
+++ src/test/java/dao/DataDAOTest.java
@@ -2,6 +2,7 @@ package dao;
 
 import dto.Data;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -15,20 +16,30 @@ public class DataDAOTest {
     DataDAO dataDAO = new DataDAO(login, password);
     private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/pogodynkaTest?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     LocalDate currentlyDate = LocalDate.parse("2018-05-05");
-    Data data = new Data(1,22, 23, currentlyDate);
+    Data data = new Data(1, 22, 23, currentlyDate);
+    Data data2 = new Data(28.8, 38.7, currentlyDate);
+    Data data3 = new Data(1, 22, 23, currentlyDate);
+    Data data4 = new Data(1, 22, 23, currentlyDate);
+    Data data5 = new Data(1, 22, 23, currentlyDate);
+
+    List<Data> expected = new ArrayList<>();
+
 
     @Test
     public void insert() {
 
+        expected.add(data2);
+        int result = new DataDAO(login, password).insert(data2, CONNECTION_STRING);
+        List<Data> database = dataDAO.getAll(CONNECTION_STRING);
 
+        Assert.assertTrue(result == database.size());
     }
 
     @Test
     public void getAll() {
-        List<Data> expected = new ArrayList<>();
         expected.add(data);
         List<Data> result = new DataDAO(login, password).getAll(CONNECTION_STRING);
-        result.forEach(data -> System.out.println(data));
+//        result.forEach(data -> System.out.println(data));
         Assert.assertEquals(expected, result);
     }
 
