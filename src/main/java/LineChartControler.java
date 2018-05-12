2df339dfbe3a62417d46d815132f92af6c10ffa8
Daniel <daniel.olszewski@int.pl>
2018-05-12 15:42:34 +0200

    modified controler

--- src/main/java/LineChartControler.java
+++ src/main/java/LineChartControler.java
@@ -2,11 +2,14 @@ import dto.Data;
 import dto.SensorService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.List;
 
@@ -85,49 +88,38 @@ public class LineChartControler {
         humi_1.setText(Double.toString(last7Days.get(5).getHumidity()));
         humi_0.setText(Double.toString(last7Days.get(6).getHumidity()));
 
+
+
+
+
+        LineChart.setTitle("Last Week");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("Temp");
+
+        series1.getData().add(new XYChart.Data("6 day ago", last7Days.get(0).getTemperature()));
+        series1.getData().add(new XYChart.Data("5 day ago", last7Days.get(1).getTemperature()));
+        series1.getData().add(new XYChart.Data("4 day ago", last7Days.get(2).getTemperature()));
+        series1.getData().add(new XYChart.Data("3 day ago", last7Days.get(3).getTemperature()));
+        series1.getData().add(new XYChart.Data("2 day ago", last7Days.get(4).getTemperature()));
+        series1.getData().add(new XYChart.Data("Yesterday", last7Days.get(5).getTemperature()));
+        series1.getData().add(new XYChart.Data("Today", last7Days.get(6).getTemperature()));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("Humi");
+        series2.getData().add(new XYChart.Data("6 day ago", last7Days.get(0).getHumidity()));
+        series2.getData().add(new XYChart.Data("5 day ago", last7Days.get(1).getHumidity()));
+        series2.getData().add(new XYChart.Data("4 day ago", last7Days.get(2).getHumidity()));
+        series2.getData().add(new XYChart.Data("3 day ago", last7Days.get(3).getHumidity()));
+        series2.getData().add(new XYChart.Data("2 day ago", last7Days.get(4).getHumidity()));
+        series2.getData().add(new XYChart.Data("Yesterday", last7Days.get(5).getHumidity()));
+        series2.getData().add(new XYChart.Data("Today", last7Days.get(6).getHumidity()));
+
+
+        LineChart.getData().addAll(series1, series2);
+
     }
-
 }
-
-
-//    public void start(Stage stage) {
-//        stage.setTitle("Show Last Week");
-//        final CategoryAxis xAxis = new CategoryAxis();
-//        final NumberAxis yAxis = new NumberAxis();
-//        xAxis.setLabel("Month");
-//        final LineChart<String, Number> lineChart =
-//                new LineChart<String, Number>(xAxis, yAxis);
-//
-//        lineChart.setTitle("Last Week");
-//
-//        XYChart.Series series1 = new XYChart.Series();
-//        series1.setName("Temp");
-//
-//        series1.getData().add(new XYChart.Data("7 day ago", 23));
-//        series1.getData().add(new XYChart.Data("6 day ago", 14));
-//        series1.getData().add(new XYChart.Data("5 day ago", 15));
-//        series1.getData().add(new XYChart.Data("4 day ago", 24));
-//        series1.getData().add(new XYChart.Data("3 day ago", 34));
-//        series1.getData().add(new XYChart.Data("2 day ago", 36));
-//        series1.getData().add(new XYChart.Data("Yesterday", 22));
-//        series1.getData().add(new XYChart.Data("Today", 45));
-//
-//        XYChart.Series series2 = new XYChart.Series();
-//        series2.setName("Humi");
-//        series2.getData().add(new XYChart.Data("7 day ago", 55));
-//        series2.getData().add(new XYChart.Data("6 day ago", 68));
-//        series2.getData().add(new XYChart.Data("5 day ago", 61));
-//        series2.getData().add(new XYChart.Data("4 day ago", 54));
-//        series2.getData().add(new XYChart.Data("3 day ago", 70));
-//        series2.getData().add(new XYChart.Data("2 day ago", 75));
-//        series2.getData().add(new XYChart.Data("Yesterday", 55));
-//        series2.getData().add(new XYChart.Data("Today", 70));
-//
-//        Scene scene  = new Scene(lineChart,500,300);
-//        lineChart.getData().addAll(series1, series2);
-//
-//        stage.setScene(scene);
-//        stage.show();
 //    }
 //
 //    public static void main(String[] args) {
