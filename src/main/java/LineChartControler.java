95caa6a136f3b9a449010c2d1cf37d58a83c300a
Laures <laures666@wp.pl>
2018-05-26 11:42:37 +0200

    Working on bugs in LineCarth part2 - second time look good

--- src/main/java/LineChartControler.java
+++ src/main/java/LineChartControler.java
@@ -71,31 +71,35 @@ public class LineChartControler {
     void onClick1(ActionEvent event) {
         SensorService sensorService = new SensorService();
         List<Data> last7Days = sensorService.getRecent7DaysFrom12OClock();
-          if(last7Days.size()<7){
-              LineChart.setTitle("Brak danych");
-          }
+        if(last7Days.size()<7){
+            LineChart.setTitle("Brak danych");
+        }
           else {
-              temp_6.setText(Double.toString(last7Days.get(0).getTemperature()));
-              temp_5.setText(Double.toString(last7Days.get(1).getTemperature()));
-              temp_4.setText(Double.toString(last7Days.get(2).getTemperature()));
-              temp_3.setText(Double.toString(last7Days.get(3).getTemperature()));
-              temp_2.setText(Double.toString(last7Days.get(4).getTemperature()));
-              temp_1.setText(Double.toString(last7Days.get(5).getTemperature()));
-              temp_0.setText(Double.toString(last7Days.get(6).getTemperature()));
-              humi_6.setText(Double.toString(last7Days.get(0).getHumidity()));
-              humi_5.setText(Double.toString(last7Days.get(1).getHumidity()));
-              humi_4.setText(Double.toString(last7Days.get(2).getHumidity()));
-              humi_3.setText(Double.toString(last7Days.get(3).getHumidity()));
-              humi_2.setText(Double.toString(last7Days.get(4).getHumidity()));
-              humi_1.setText(Double.toString(last7Days.get(5).getHumidity()));
-              humi_0.setText(Double.toString(last7Days.get(6).getHumidity()));
+            temp_6.setText(Double.toString(last7Days.get(0).getTemperature()));
+            temp_5.setText(Double.toString(last7Days.get(1).getTemperature()));
+            temp_4.setText(Double.toString(last7Days.get(2).getTemperature()));
+            temp_3.setText(Double.toString(last7Days.get(3).getTemperature()));
+            temp_2.setText(Double.toString(last7Days.get(4).getTemperature()));
+            temp_1.setText(Double.toString(last7Days.get(5).getTemperature()));
+            temp_0.setText(Double.toString(last7Days.get(6).getTemperature()));
+            humi_6.setText(Double.toString(last7Days.get(0).getHumidity()));
+            humi_5.setText(Double.toString(last7Days.get(1).getHumidity()));
+            humi_4.setText(Double.toString(last7Days.get(2).getHumidity()));
+            humi_3.setText(Double.toString(last7Days.get(3).getHumidity()));
+            humi_2.setText(Double.toString(last7Days.get(4).getHumidity()));
+            humi_1.setText(Double.toString(last7Days.get(5).getHumidity()));
+            humi_0.setText(Double.toString(last7Days.get(6).getHumidity()));
 
-              LineChart.setTitle("Last Week");
+            final CategoryAxis xAxis = new CategoryAxis();
+            final NumberAxis yAxis = new NumberAxis();
 
-              XYChart.Series series1 = new XYChart.Series();
-              series1.setName("Temp");
+            LineChart<String,Number> lineChart = new LineChart<>(xAxis, yAxis);
+            lineChart.setTitle("Last Week");
 
-              series1.getData().add(new XYChart.Data("6 day ago", last7Days.get(0).getTemperature()));
+            XYChart.Series series1 = new XYChart.Series();
+            series1.setName("Temp");
+
+            series1.getData().add(new XYChart.Data("6 day ago", last7Days.get(0).getTemperature()));
               series1.getData().add(new XYChart.Data("5 day ago", last7Days.get(1).getTemperature()));
               series1.getData().add(new XYChart.Data("4 day ago", last7Days.get(2).getTemperature()));
               series1.getData().add(new XYChart.Data("3 day ago", last7Days.get(3).getTemperature()));
